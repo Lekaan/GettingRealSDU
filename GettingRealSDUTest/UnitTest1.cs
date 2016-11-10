edebb66aaf584ab2b0492758356b454a594b75d2
kaans <kaans@DESKTOP-U4CBDT5>
2016-11-10 10:33:03 +0100

    Add project files.

---
+++ GettingRealSDUTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GettingRealSDUTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
