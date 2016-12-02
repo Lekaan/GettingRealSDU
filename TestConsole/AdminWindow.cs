688ac4365e0d46117e6e64d92807456eff02af60
Pelle <Pelle@LAPTOP-T69PBCOE>
2016-12-02 12:13:25 +0100

    ff

--- TestConsole/AdminWindow.cs
+++ TestConsole/AdminWindow.cs
@@ -3,12 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GettingRealSDU;
 
 namespace TestConsole
 {
     class AdminWindow
     {
+        public void MainWindowsAdmin()
+        {
+            Setup.Headder();
+            Setup.ShowWhoIsLoggedIn();
+            Console.WriteLine("Choose between: 1: Adding Device \n 3: Delete Device");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
 
+
+
+                    break;
+
+                case "3":
+
+
+                    break;
+
+            }
+
+
+
+        }
 
     }
 }
